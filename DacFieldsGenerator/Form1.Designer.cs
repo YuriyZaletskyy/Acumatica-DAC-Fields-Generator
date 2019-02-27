29faba1f1f253b6b09eea354aed3bea6899f8037
Yuriy Zaletskyy <yura_zaletsiy@yahoo.com>
2019-02-27 14:46:07 +0200

    implemented DAC Fields generator for PXFilter

---
+++ DacFieldsGenerator/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace DacFieldsGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtMember = new System.Windows.Forms.TextBox();
+            this.lblDataMemberName = new System.Windows.Forms.Label();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.lblType = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUIField = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(51, 160);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtMember
+            // 
+            this.txtMember.Location = new System.Drawing.Point(51, 37);
+            this.txtMember.Name = "txtMember";
+            this.txtMember.Size = new System.Drawing.Size(175, 20);
+            this.txtMember.TabIndex = 1;
+            // 
+            // lblDataMemberName
+            // 
+            this.lblDataMemberName.AutoSize = true;
+            this.lblDataMemberName.Location = new System.Drawing.Point(48, 21);
+            this.lblDataMemberName.Name = "lblDataMemberName";
+            this.lblDataMemberName.Size = new System.Drawing.Size(99, 13);
+            this.lblDataMemberName.TabIndex = 2;
+            this.lblDataMemberName.Text = "Data member name";
+            // 
+            // cmbType
+            // 
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Items.AddRange(new object[] {
+            "int?",
+            "string",
+            "decimal?",
+            "DateTime?"});
+            this.cmbType.Location = new System.Drawing.Point(51, 83);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(175, 21);
+            this.cmbType.TabIndex = 3;
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(48, 67);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(66, 13);
+            this.lblType.TabIndex = 4;
+            this.lblType.Text = "Choose type";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "UI Field";
+            // 
+            // txtUIField
+            // 
+            this.txtUIField.Location = new System.Drawing.Point(51, 134);
+            this.txtUIField.Name = "txtUIField";
+            this.txtUIField.Size = new System.Drawing.Size(175, 20);
+            this.txtUIField.TabIndex = 5;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(275, 37);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(479, 401);
+            this.txtOutput.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUIField);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.cmbType);
+            this.Controls.Add(this.lblDataMemberName);
+            this.Controls.Add(this.txtMember);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtMember;
+        private System.Windows.Forms.Label lblDataMemberName;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUIField;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ DacFieldsGenerator/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DacFieldsGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            var region = txtMember.Text.Replace("(", "").Replace(")", "").Replace(" ", "");
+            var absClassName = region[0].ToString().ToLower() + region.Substring(1);
+
+            var attributes = $"[PXUIField(DisplayName =  \"{region}\")]\r\n";
+            if (txtUIField.Text != "")
+            {
+                attributes = $"[PXUIField(DisplayName = \"{txtUIField.Text}\")]\r\n";
+            }
+
+            string attrAdd = string.Empty;
+            if (cmbType.SelectedItem.ToString() == "int?")
+            {
+                attrAdd = "[PXInt]";
+            }
+            if (cmbType.SelectedItem.ToString() == "string")
+            {
+                attrAdd = "[PXString]";
+            }
+            if (cmbType.SelectedItem.ToString() == "decimal?")
+            {
+                attrAdd = "[PXDecimal]";
+            }
+            if (cmbType.SelectedItem.ToString() == "DateTime?")
+            {
+                attrAdd = "[PXDate()]";
+            }
+            attributes = attributes + attrAdd;
+            var memberName = region[0].ToString().ToUpper() + region.Substring(1);
+
+            //var output = string.Format(template, region, absClassName, attributes); // , cmbType.SelectedItem.ToString(), memberName);
+            
+            var output = $"#region {region}\r\npublic abstract class {absClassName} :  PX.Data.IBqlField\r\n{{\r\n}}\r\n\r\n " +
+                         $"{attributes} " +
+                         $"\r\npublic virtual {cmbType.SelectedItem.ToString()} {memberName}\r\n{{\r\nget;\r\nset;\r\n}}\r\n#endregion";
+
+
+            txtOutput.Text = txtOutput.Text + output;
+
+        }
+    }
+}
